09e196f41db6407714f788f530cf574082de5ebf
Tommy Tran <tommytran@tommys-macbook-pro.local>
2018-09-26 08:05:28 -0700

    Initial Commit

---
+++ InstaFy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaFy
+//
+//  Created by Tommy Tran on 26/09/2018.
+//  Copyright © 2018 Tommy Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
